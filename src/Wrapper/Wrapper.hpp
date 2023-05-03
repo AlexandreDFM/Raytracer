4d81bd11aa02730ac0917d25f3e4ac73511c5fa9
Neeptossss <gwenael.hubler@epitech.eu>
2023-05-03 13:56:56 +0200

    add: wrapper for primitives libs

--- src/Wrapper/Wrapper.hpp
+++ src/Wrapper/Wrapper.hpp
@@ -8,15 +8,29 @@
 #ifndef WRAPPER_HPP_
 	#define WRAPPER_HPP_
 
-namespace raytracer {
+#include <dlfcn.h>
+#include <iostream>
+#include <string>
+
+namespace RayTracer {
     class Wrapper {
     public:
         Wrapper();
 
         ~Wrapper();
 
+        template<typename T>
+        T *getFunction(const std::string &name)
+        {
+            T *function = reinterpret_cast<T *>(dlsym(_handle, name.c_str()));
+            if (!function)
+                std::cerr << "Cannot load symbol: " << dlerror() << '\n';
+            return function;
+        }
+
     protected:
     private:
+        void *_handle = nullptr;
     };
 }
 
