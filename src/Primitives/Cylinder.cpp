1046dc13a45397a5502f0f5b3e45ccb9173a5677
Neeptossss <gwenael.hubler@epitech.eu>
2023-05-15 17:11:56 +0200

    add: infinite cylinder

--- src/Primitives/Cylinder.cpp
+++ src/Primitives/Cylinder.cpp
@@ -8,62 +8,52 @@
 #include "Cylinder.hpp"
 
 namespace RayTracer {
-    Cylinder::Cylinder(double _radius, double _y0, double _y1, const Vector3D &_position, std::shared_ptr<IMaterial> &_mat)
-            : radius(_radius), y0(_y0), y1(_y1), position(_position), matPtr(_mat) {}
-
-    bool Cylinder::hit(const RayTracer::Ray& r, double tMin, double tMax, RayTracer::hitRecord& rec) const {
-        Vector3D oc = r.origin() - position;
-
-        double a = r.direction().x() * r.direction().x() + r.direction().z() * r.direction().z();
-        double b = 2.0 * (oc.x() * r.direction().x() + oc.z() * r.direction().z());
-        double c = oc.x() * oc.x() + oc.z() * oc.z() - radius * radius;
-
-        double discriminant = b * b - 4.0 * a * c;
-
-        if (discriminant > 0) {
-            double root = (-b - sqrt(discriminant)) / (2.0 * a);
-            if (root < tMin || root > tMax) {
-                root = (-b + sqrt(discriminant)) / (2.0 * a);
-                if (root < tMin || root > tMax) {
-                    return false;
-                }
-            }
-
-            double y = r.origin().y() + root * r.direction().y();
-            if (y < y0 || y > y1) {
-                return false;
-            }
-
-            rec.t = root;
-            rec.p = r.at(rec.t);
-            Vector3D outward_normal = (rec.p - position) / radius;
-            rec.set_face_normal(r, outward_normal);
-            rec.matPtr = matPtr;
-
-            return true;
-        }
-
-        return false;
+    Cylinder::Cylinder(Point3D baseCenter, double height, double radius, std::shared_ptr<IMaterial>& m)
+            : baseCenter(baseCenter), height(height), radius(radius), matPtr(m)
+    {
     }
 
-    bool Cylinder::boundingBox(double time0, double time1, AxisAlignedBoundBox &outputBox) {
-        outputBox = AxisAlignedBoundBox(
-                position - Vector3D(radius, y0, radius),
-                position + Vector3D(radius, y1, radius)
-        );
+    bool Cylinder::hit(const RayTracer::Ray& r, double tMin, double tMax, hitRecord& rec) const
+    {
+        Vector3D oc = r.origin() - baseCenter;
+
+        auto a = Vector3D::dot(r.direction(), r.direction()) - Vector3D::dot(r.direction(), Vector3D(0, 1, 0)) * Vector3D::dot(r.direction(), Vector3D(0, 1, 0));
+        auto b = 2 * (Vector3D::dot(oc, r.direction()) - Vector3D::dot(oc, Vector3D(0, 1, 0)) * Vector3D::dot(r.direction(), Vector3D(0, 1, 0)));
+        auto c = Vector3D::dot(oc, oc) - radius * radius - Vector3D::dot(oc, Vector3D(0, 1, 0)) * Vector3D::dot(oc, Vector3D(0, 1, 0));
+
+        auto discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0)
+            return false;
+
+        auto sqrtd = std::sqrt(discriminant);
+
+        // Find the nearest root that lies in the acceptable range.
+        auto root = (-b - sqrtd) / (2 * a);
+        if (root < tMin || tMax < root) {
+            root = (-b + sqrtd) / (2 * a);
+            if (root < tMin || tMax < root)
+                return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        Vector3D outward_normal = (rec.p - baseCenter - Vector3D(0, 1, 0) * (height / 2)) / radius;
+        rec.set_face_normal(r, outward_normal);
+        rec.matPtr = matPtr;
+
         return true;
     }
 
     extern "C" {
-        RayTracer::IPrimitive *entryPoint(Point3D &center, std::vector<double> variables, std::shared_ptr<RayTracer::IMaterial> &matPtr)
-        {
-            return new Cylinder(variables[0], variables[1], variables[2], center, matPtr);
-        }
+    IPrimitive* entryPoint(Point3D baseCenter, std::vector<double> variables, std::shared_ptr<RayTracer::IMaterial>& matPtr)
+    {
+        return new RayTracer::Cylinder(baseCenter, variables[0], variables[1], matPtr);
+    }
 
-        char *getType()
-        {
-            return (char *)"primitive_cylinder";
-        }
+    char* getType()
+    {
+        return (char*)"primitive_cylinder";
+    }
     }
 }
-
